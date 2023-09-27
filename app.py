da11797fba9b393e209bf597e7867c68b72a7315
Kevin Beier <kabeier@gmail.com>
2023-09-27 01:16:38 -0400

    Step 2-caching

--- app.py
+++ app.py
@@ -1,93 +1,136 @@
-# Import necessary modules from the Flask and Redis libraries
+# Import necessary modules
 from flask import Flask, render_template, jsonify, request
 from redis import Redis
 import timeit
 import math
+from flask_sqlalchemy import SQLAlchemy
+from flask_caching import Cache
 
-# Initialize the Flask application
+# Initialize Flask app
 app = Flask(__name__)
 
-# Initialize a Redis client to connect to a local Redis server
+# Initialize Redis instance
+# Connect to Redis server running on localhost at port 6379
 redis = Redis(host='localhost', port=6379, decode_responses=True)
 
-# Define the root route for the web application
+# Configure SQLAlchemy database URI
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///demo.db'
+
+# Configure flask-caching to use Redis
+# CACHE_TYPE specifies the backend to use, in this case, Redis.
+# CACHE_REDIS_URL specifies the URL to connect to the Redis server.
+app.config['CACHE_TYPE'] = 'redis'
+app.config['CACHE_REDIS_URL'] = 'redis://localhost:6379/0'
+
+# Initialize SQLAlchemy and Cache instances
+db = SQLAlchemy(app)
+cache = Cache(app)
+
+# Define User model for SQLAlchemy
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "username": self.username
+        }
+
+# Create tables and add a test user.
+# This function is meant to be run in the Flask shell
+def create_tables():
+    db.create_all()
+    new_user = User(username="john")
+    db.session.add(new_user)
+    db.session.commit()
+
+# Memoize the get_user_from_db function with a 60-second timeout
+# This means that returned values are cached for 60 seconds.
+@cache.memoize(timeout=60)
+def get_user_from_db(user_id):
+    user = User.query.get(user_id)
+    if user:
+        return user.to_dict()
+    return None
+
+# Define a route for getting user data by ID
+@app.route('/user/<int:user_id>')
+def get_user(user_id):
+    # get_user_from_db is called, but due to caching, if the data is already
+    # in the cache, it will be returned without hitting the database.
+    user_data = get_user_from_db(user_id)
+    if user_data:
+        return jsonify({"source": "database", "user": user_data})
+    else:
+        return jsonify({"error": "User not found"}), 404
+
 @app.route('/')
 def index():
-    # Render the index.html template
     return render_template('index.html')
 
-# A slow calculation function that simulates time-consuming operations
 def slow_calculation():
     return sum(math.sqrt(i) for i in range(1, 1000000))
 
-# A fast calculation function that uses Redis caching to speed up operations
 def fast_calculation():
-    # Check if the result exists in the Redis cache
     result = redis.get('my_key')
     if result is None:
-        # If not, perform the calculation and save it to the cache
         result = sum(math.sqrt(i) for i in range(1, 1000000))
         redis.set('my_key', result)
     else:
-        # Convert the cached result to a float
         result = float(result)
     return result
 
-# Define the /fast route
 @app.route('/fast')
 def fast_route():
-    # Measure the time taken for the fast calculation using timeit
     elapsed_time = timeit.timeit('fast_calculation()', globals=globals(), number=1)
     result = fast_calculation()
     return {"result": f"Result {result}", "source": "Redis", "time": f"{elapsed_time:.6f} seconds"}
 
-# Define the /slow route
 @app.route('/slow')
 def slow_route():
-    # Measure the time taken for the slow calculation using timeit
     elapsed_time = timeit.timeit('slow_calculation()', globals=globals(), number=1)
     result = slow_calculation()
     return {"result": f"Result {result}", "source": "Python", "time": f"{elapsed_time:.6f} seconds"}
 
-# Define the /lists route to demonstrate Redis Lists
 @app.route('/lists')
 def redis_lists():
-    # Add elements to a Redis list using LPUSH and RPUSH
+    # Push elements to a list
     redis.lpush('my_list', 'a')
     redis.lpush('my_list', 'b')
     redis.lpush('my_list', 'c')
     redis.rpush('my_list', 'x')
 
-    # Retrieve elements from the Redis list using LRANGE
+    # Retrieve elements from a list
     elements = redis.lrange('my_list', 0, -1)
+    print(elements) 
     return elements
 
-# Define the /hashset route to demonstrate Redis HashSets
+
 @app.route('/hashset')
 def redis_hashsets():
-    # Set fields in a Redis hash using HSET
+    # Set fields in a hash
     redis.hset('my_hash', 'key', 'value')
     redis.hset('my_hash', 'name', 'John')
 
-    # Retrieve fields from the Redis hash using HGET
+    # Retrieve fields from a hash
     value1 = redis.hget('my_hash', 'key')
     value2 = redis.hget('my_hash', 'name')
-    return {"value1": value1, "value2": value2}
+    print(value1, value2)  
+    return {"value1":value1,"value2":value2}
 
-# Define the /count route to demonstrate Redis counters
 @app.route('/count')
 def page_view():
-    # Increment the pageview count using Redis INCR
     redis.incr('pageview_count')
     return {"pageviews": redis.get('pageview_count')}
 
-# Define the /flush route to clear the Redis cache
 @app.route('/flush')
 def flush():
-    # Flush the current Redis database using FLUSHDB
     redis.flushdb()
     return "flushed"
 
-# Start the Flask development server
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
